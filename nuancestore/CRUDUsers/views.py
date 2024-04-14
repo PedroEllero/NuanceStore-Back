e7995b2136d7d54fec54674719b9bd03dd095dd8
HugoOkumura <okumura@alunos.utfpr.edu.br>
2024-04-14 01:41:32 -0300

    tirando comentarios

--- nuancestore/CRUDUsers/views.py
+++ nuancestore/CRUDUsers/views.py
@@ -1,4 +1,3 @@
-# from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -38,19 +37,3 @@ def users_detail(request,pk):
     elif request.method == 'DELETE':
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-# from .UsersForm import UsersForm
-
-# def create_view(request):
-#     context = {}
-
-#     form = UsersForm(request.POST or None)
-
-#     if form.is_valid():
-#         form.save()
-
-#     context['form']=form
-#     return render(request, "", context)
-
