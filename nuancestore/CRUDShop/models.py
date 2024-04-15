5e8469ae33940db324ad710f43c542c63c7651c8
ruanazevedo <ruannzeroh@gmail.com>
2024-04-15 16:38:27 -0300

    CRUDShop atualizado

--- nuancestore/CRUDShop/models.py
+++ nuancestore/CRUDShop/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-#  class Tipo(models.Model):
-#      descricao = models.CharField(max_length=50)
+class Tipo(models.Model):
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.descricao
 
 class Produto(models.Model):
     preco = models.DecimalField(decimal_places=2, max_digits=12)
     descricao = models.CharField(max_length=200)
-    Tipo = models.CharField(max_length=50)
-#   IDTipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    IDTipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.descricao
--- nuancestore/CRUDShop/serializers.py
+++ nuancestore/CRUDShop/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from.models import *
 
-# class Tipo(serializers.ModelSerializer):
-#     class Meta:
-#         model = Tipo
-#         fields = ('descricao')
+class Tipo(serializers.ModelSerializer):
+     class Meta:
+         model = Tipo
+         fields = ('categoria')
 
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
         fields = (
-            # 'IDTipo',
+            'IDTipo',
             'descricao',
             'preco',
-            'Tipo'
+            'Tipo',
+            'nome',
             )
